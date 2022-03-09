8ec4059ccd6d4f2dab7bde4413ece82f925862b9
AloshaFromRivia <alex-iv999.ivanov@yandex.ru>
2022-03-09 20:09:10 +0300

    Добавьте файлы проекта.

---
+++ RideAndDestroy/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RideAndDestroy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Title = "Главная";
+            return View();
+        }
+    }
+}
